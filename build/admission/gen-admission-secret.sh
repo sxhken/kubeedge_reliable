6be5070222156b2431a812368d9af8ff26a5f5b3
fisherxu <xufei40@huawei.com>
2019-08-26 17:59:33 +0800

    fix build

--- build/admission/gen-admission-secret.sh
+++ build/admission/gen-admission-secret.sh
@@ -13,7 +13,6 @@ if [[ ! -x "$(command -v openssl)" ]]; then
     exit 1
 fi
 
-csrName=${SERVICE}.${NAMESPACE}
 mkdir -p ${CERTDIR}
 echo "creating certs in dir ${CERTDIR} "
 
@@ -33,63 +32,24 @@ DNS.2 = ${SERVICE}.${NAMESPACE}
 DNS.3 = ${SERVICE}.${NAMESPACE}.svc
 EOF
 
-openssl genrsa -out ${CERTDIR}/server-key.pem 2048
-openssl req -new -key ${CERTDIR}/server-key.pem -subj "/CN=${SERVICE}.${NAMESPACE}.svc" -out ${CERTDIR}/server.csr -config ${CERTDIR}/csr.conf
+openssl genrsa -out ${CERTDIR}/ca.key 2048
+openssl req -x509 -new -nodes -key ${CERTDIR}/ca.key -subj "/CN=${SERVICE}.${NAMESPACE}.svc" -out ${CERTDIR}/ca.crt
 
-# clean-up any previously created CSR for our service. Ignore errors if not present.
-kubectl delete csr ${csrName} 2>/dev/null || true
+openssl genrsa -out ${CERTDIR}/server.key 2048
+openssl req -new -key ${CERTDIR}/server.key -subj "/CN=${SERVICE}.${NAMESPACE}.svc" -out ${CERTDIR}/server.csr -config ${CERTDIR}/csr.conf
 
-# create  server cert/key CSR and  send to k8s API
-cat <<EOF | kubectl create -f -
-apiVersion: certificates.k8s.io/v1beta1
-kind: CertificateSigningRequest
-metadata:
-  name: ${csrName}
-spec:
-  groups:
-  - system:authenticated
-  request: $(cat ${CERTDIR}/server.csr | base64 | tr -d '\n')
-  usages:
-  - digital signature
-  - key encipherment
-  - server auth
-EOF
-
-# verify CSR has been created
-while true; do
-    kubectl get csr ${csrName}
-    if [ "$?" -eq 0 ]; then
-        break
-    fi
-done
-
-# approve and fetch the signed certificate
-kubectl certificate approve ${csrName}
-# verify certificate has been signed
-for x in $(seq 20); do
-    serverCert=$(kubectl get csr ${csrName} -o jsonpath='{.status.certificate}')
-    if [[ ${serverCert} != '' ]]; then
-        break
-    fi
-    sleep 1
-done
-if [[ ${serverCert} == '' ]]; then
-    echo "ERROR: After approving csr ${csrName}, the signed certificate did not appear on the resource. Giving up after 20 attempts." >&2
-    exit 1
-fi
-echo ${serverCert} | openssl base64 -d -A -out ${CERTDIR}/server-cert.pem
-
-# ca cert
-kubectl get configmap -n kube-system extension-apiserver-authentication -o=jsonpath='{.data.client-ca-file}' > ${CERTDIR}/ca-cert.pem
+openssl x509 -req -in  ${CERTDIR}/server.csr -CA  ${CERTDIR}/ca.crt -CAkey  ${CERTDIR}/ca.key \
+-CAcreateserial -out  ${CERTDIR}/server.crt \
+-extensions v3_req -extfile  ${CERTDIR}/csr.conf
 
 if [[ "${ENABLE_CREATE_SECRET}" = true ]]; then
     kubectl get ns ${NAMESPACE} || kubectl create ns ${NAMESPACE}
 
     # create the secret with CA cert and server cert/key
     kubectl create secret generic ${SECRET} \
-        --from-file=tls.key=${CERTDIR}/server-key.pem \
-        --from-file=tls.crt=${CERTDIR}/server-cert.pem \
-        --from-file=ca.crt=${CERTDIR}/ca-cert.pem \
+        --from-file=tls.key=${CERTDIR}/server.key \
+        --from-file=tls.crt=${CERTDIR}/server.crt \
+        --from-file=ca.crt=${CERTDIR}/ca.crt \
         --dry-run -o yaml |
     kubectl -n ${NAMESPACE} apply -f -
 fi
